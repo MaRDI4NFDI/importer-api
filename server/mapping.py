11a9deb72389b158d92ac4b6f6badecc41298efc
Eloi Ferrer <ferrer@zib.de>
2025-04-03 12:05:21 +0000

    update db schema

--- server/mapping.py
+++ server/mapping.py
@@ -22,31 +22,43 @@ def get_mapping(entity_id):
 def get_local_id(wikidata_id):
     engine = create_engine()
     metadata = db.MetaData()
+    table_name = 'items'
+    if wikidata_id.startswith('P'):
+        table_name = 'properties'
     table = db.Table(
-        "wb_id_mapping", 
+        table_name, 
         metadata, 
         autoload_with=engine
     )
     sql = db.select(table.columns['local_id']).where(
-        table.columns.wikidata_id == wikidata_id,
+        table.columns.wikidata_id == wikidata_id[1:],
     )
     with engine.connect() as connection:
         db_result = connection.execute(sql).fetchone()
     if db_result:
-        return db_result[0]
+        if wikidata_id.startswith('Q'):
+            return f"Q{db_result[0]}"
+        else:
+            return f"P{db_result[0]}"
 
 def get_wikidata_id(local_id):
     engine = create_engine()
     metadata = db.MetaData()
+    table_name = 'items'
+    if wikidata_id.startswith('P'):
+        table_name = 'properties'
     table = db.Table(
         "wb_id_mapping", 
         metadata, 
         autoload_with=engine
     )
     sql = db.select(table.columns['wikidata_id']).where(
-        table.columns.local_id == local_id,
+        table.columns.local_id == local_id[1:],
     )
     with engine.connect() as connection:
         db_result = connection.execute(sql).fetchone()
     if db_result:
-        return db_result[0]
+        if wikidata_id.startswith('Q'):
+            return f"Q{db_result[0]}"
+        else:
+            return f"P{db_result[0]}"
