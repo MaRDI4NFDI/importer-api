2bcd269daaf8be8fa63f18ad1a5da248aee983ee
Eloi Ferrer <ferrer@zib.de>
2025-10-13 07:44:18 +0000

    search alias by default

--- server/search.py
+++ server/search.py
@@ -12,43 +12,47 @@ def search_items(label):
         is_truncated = True
 
     engine = create_engine(mediawiki=True)
-    with engine.connect() as connection:
-        metadata = db.MetaData()
-        try:
-            wbt_item_terms = db.Table(
-                "wbt_item_terms", metadata, autoload_with=connection
-            )
-            wbt_term_in_lang = db.Table(
-                "wbt_term_in_lang", metadata, autoload_with=connection
-            )
-            wbt_text_in_lang = db.Table(
-                "wbt_text_in_lang", metadata, autoload_with=connection
-            )
-            wbt_text = db.Table(
-                "wbt_text", metadata, autoload_with=connection
-            )
-            query = (db.select(wbt_item_terms.columns.wbit_item_id)
-                    .join(wbt_term_in_lang, wbt_item_terms.columns.wbit_term_in_lang_id == wbt_term_in_lang.columns.wbtl_id)
-                    .join(wbt_text_in_lang, wbt_term_in_lang.columns.wbtl_text_in_lang_id == wbt_text_in_lang.columns.wbxl_id)
-                    .join(wbt_text, wbt_text.columns.wbx_id == wbt_text_in_lang.columns.wbxl_text_id)
-                    .where(db.and_(
-                                case(
-                                    (is_truncated, wbt_text.columns.wbx_text.like(label + b"%")),
-                                    else_=wbt_text.columns.wbx_text == label),
-                                wbt_term_in_lang.columns.wbtl_type_id == 1,
-                                wbt_text_in_lang.columns.wbxl_language == bytes("en", "utf-8"))))
-            results = connection.execute(query).fetchall()
-            entity_id = []
-            if results:
-                for result in results:
-                    entity_id.append(f"Q{str(result[0])}")
 
-        except Exception as e:
-            raise Exception(
-                "Error attempting to read mappings from database\n{}".format(e)
-            )
+    def query_wikidata_table(field_type):
+        # field_type = 1 : Label
+        # field_type = 2 : Alias
+        # see: https://doc.wikimedia.org/Wikibase/REL1_41/php/docs_sql_wbt_type.html
+        entity_id = []
+        with engine.connect() as connection:
+            metadata = db.MetaData()
+            try:            
+                wbt_item_terms = db.Table(
+                    "wbt_item_terms", metadata, autoload_with=connection
+                )
+                wbt_term_in_lang = db.Table(
+                    "wbt_term_in_lang", metadata, autoload_with=connection
+                )
+                wbt_text_in_lang = db.Table(
+                    "wbt_text_in_lang", metadata, autoload_with=connection
+                )
+                wbt_text = db.Table(
+                    "wbt_text", metadata, autoload_with=connection
+                )
+                query = (db.select(wbt_item_terms.columns.wbit_item_id)
+                        .join(wbt_term_in_lang, wbt_item_terms.columns.wbit_term_in_lang_id == wbt_term_in_lang.columns.wbtl_id)
+                        .join(wbt_text_in_lang, wbt_term_in_lang.columns.wbtl_text_in_lang_id == wbt_text_in_lang.columns.wbxl_id)
+                        .join(wbt_text, wbt_text.columns.wbx_id == wbt_text_in_lang.columns.wbxl_text_id)
+                        .where(and_(
+                                    case(
+                                        (is_truncated, wbt_text.columns.wbx_text.like(label + b"%")),
+                                        else_=wbt_text.columns.wbx_text == label), 
+                                    wbt_term_in_lang.columns.wbtl_type_id == field_type,
+                                    wbt_text_in_lang.columns.wbxl_language == bytes("en", "utf-8"))))
+                results = connection.execute(query).fetchall()
+                return [f"Q{str(result[0])}" for result in results]
+            except Exception as e:
+                raise Exception(
+                    "Error attempting to read mappings from database\n{}".format(e)
+                )
         
-        return {'QID': entity_id}
+    entity_id = query_wikidata_table(field_type=1)
+    entity_id += query_wikidata_table(field_type=2)
+    return {'QID': entity_id}
 
 def search_properties(label):
     label = urllib.parse.unquote(label)
